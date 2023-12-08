bceb27f52c882be31baa6d38df11af0c54a5a8be
SlavaV79 <82697007+SlavaV79@users.noreply.github.com>
2023-12-08 21:44:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// console.log("Hello world!");
+// console.log("Hello world! #2");
+
+// alert("Hello");
+// alert("Hello #2");
+
+// prompt("Как вас зовут?", "Петя");
+
+console.log("Запустить таймер");
+
+alert("5");
+alert("4");
+alert("3");
+alert("2");
+alert("1");
+alert("0");
+
+alert("Обратный отчёт закончен!");
