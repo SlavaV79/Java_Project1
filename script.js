67fa02979a8754aa0de0568ecdd1a595246390e7
SlavaV79 <82697007+SlavaV79@users.noreply.github.com>
2023-12-08 23:03:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+let myName = "Слава";
+let mySurname = "Вакаров";
+let myFavoriteDrink = "Тан, Айран";
+let myFavoriteAnimal = "Собаки, кошки. Вообще всех люблю";
+let myFavoriteProgrammingLanguage = "JavaScript";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
+
+
+
+
+
+// let, const
+/*
+let name = "Alex";
+const lang = "Java Script";
+name = "Petya";
+
+
+console.log(name);
+console.log(lang);
+*/
+
+
+
+
 // console.log("Hello world!");
 // console.log("Hello world! #2");
 
@@ -6,13 +36,14 @@
 
 // prompt("Как вас зовут?", "Петя");
 
-console.log("Запустить таймер");
+// console.log("Запустить таймер");
 
-alert("5");
-alert("4");
-alert("3");
-alert("2");
-alert("1");
-alert("0");
+// alert("5");
+// alert("4");
+// alert("3");
+// alert("2");
+// alert("1");
+// alert("0");
+// alert("Обратный отчёт закончен!");
 
-alert("Обратный отсчет закончен!");
+// window.alert("Hello world");
